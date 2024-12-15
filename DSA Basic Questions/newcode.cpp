a528ffb5dd6ad47f3aaa4e90a81e443f65892f5e
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-12-15 20:25:39 +0530

    Assignment and initialization

--- DSA Basic Questions/newcode.cpp
+++ DSA Basic Questions/newcode.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //This is direct-line-initialization
     string n{'H','e','l','l','o'};
-    int k{2};
+    //This is copy-initialization
+    int k=2;
+    //This is direct initialization
+    int v(6);
+    //Assigning value to a variable where it is defined or decalred is initialization.
+    //This is assignment
+    int m;
+    m=6; 
+    //assigning value to the variable after the line where it has been defined or decalred is assignment.
     cout<<n<<endl;
-    cout<<k;
+    cout<<k<<"/n";
+    cout<<v<<"/n";
+    cout<<m<<"/n";
 }
