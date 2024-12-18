a6cf86e3b019416babe8d58e133602c4d9b1492c
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-12-19 01:02:08 +0530

    new questions

--- DSA Basic Questions/LargestElementInArray.cpp
+++ DSA Basic Questions/LargestElementInArray.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+using namespace std;
+int main(){
+    int n,i,highest;
+    cout<<"Enter the number of elements in the array \n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array \n";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    highest = arr[0];
+    for(i=0;i<n;i++){
+        if(arr[i] > highest){
+            highest = arr[i];
+        }
+    }
+    cout<<"The highest element is : "<<highest;
+}
