94706e2aa6eef162d24fb9aeee2b209ef42281ed
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-12-19 01:07:23 +0530

    new codes

--- DSA Basic Questions/arrayReverse.cpp
+++ DSA Basic Questions/arrayReverse.cpp
@@ -8,10 +8,11 @@ int main(){
     int l = s.length();
     char str[l+1];
     char c;
-    for(int i=0;i<l;i++){
-        c=s[(l-1)-i];
+    for(int i=0;i<l;i++) {
+        c = s[(l-1)-i];
         str[i]=c;
     }
     str[l] = '\0';
     cout<<"The resultant string is : "<<str;
+    return 0;
 }
